eeb56c764b7dd953b9753849ebe76b0b24d6016c
XAINI <shanzhenqiang0814@163.com>
2016-06-05 23:18:59 +0800

    添加了屏幕关闭锁屏界面事件

--- app/src/main/java/com/example/shanzhenqiang/lockapplication/MainActivity.java
+++ app/src/main/java/com/example/shanzhenqiang/lockapplication/MainActivity.java
@@ -7,14 +7,12 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "LockScreenActivity";
-    private SliderRelativeLayout sliderRelativeLayout;
-    private ImageView imageView;
-    public static int MSG_LOCK_SUCCESS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +23,11 @@ public class MainActivity extends AppCompatActivity {
         /** 屏蔽back键*/
         this.getWindow().setFlags(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG, WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
 
-        sliderRelativeLayout = (SliderRelativeLayout) findViewById(R.id.sliderLayout);
-        sliderRelativeLayout.setMainHandler(handler);
-
         startService(new Intent(MainActivity.this, LockScreenService.class)); //这里要显示的调用服务
     }
 
-    private Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            if (MSG_LOCK_SUCCESS == msg.what){
-                finish();
-            }
-        }
-    };
+    public void close(View view){
+        finish();
+    }
+
 }
