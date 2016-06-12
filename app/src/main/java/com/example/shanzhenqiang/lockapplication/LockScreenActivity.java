65f2093e5f73ecb1a4f0260ebf4c8da696c9f1f6
XAINI <shanzhenqiang0814@163.com>
2016-06-12 12:52:03 +0800

    添加了OkHttpClient

--- app/src/main/java/com/example/shanzhenqiang/lockapplication/LockScreenActivity.java
+++ app/src/main/java/com/example/shanzhenqiang/lockapplication/LockScreenActivity.java
@@ -1,10 +1,17 @@
 package com.example.shanzhenqiang.lockapplication;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class LockScreenActivity extends AppCompatActivity {
     private final String TAG = "LockScreenActivity";
@@ -16,10 +23,48 @@ public class LockScreenActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
         setContentView(R.layout.activity_lock_screen);
+        MyTask myTask = new MyTask();
+        myTask.execute();
     }
 
     public void close(View view){
         Log.i(TAG,"---------------You clicked unlock--------------");
         finish();
+    }
+
+    public class MyTask extends AsyncTask<String, Integer, String>{
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String result;
+            try {
+                result = get_examination();
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+        }
+    }
+    String get_examination() throws Exception{
+        String url = "http://192.168.100.3:3000/curriculums/fetch_curriculums";
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        Response response = client.newCall(request).execute();
+        if (response.isSuccessful()) {
+            return response.body().string();
+        } else {
+            throw new IOException("Unexpected code " + response);
+        }
     }
 }
